bfe0c7949fa8dd99921fde7a810a2a6c5ccc8f88
Thomas Meson <zllak@hycik.org>
2015-09-15 12:24:38 +0200

    vips_gaussblur: remove dependency on libmath
    remove ceil() on call to vips_gaussblur, simply cast to int

--- vips.h
+++ vips.h
@@ -16,9 +16,6 @@
  */
 
 #if (VIPS_MAJOR_VERSION == 7 && VIPS_MINOR_VERSION < 41)
-/* we need math.h for ceil() in vips_gaussblur_bridge */
-#include <math.h>
-
 #define VIPS_ANGLE_D0 VIPS_ANGLE_0
 #define VIPS_ANGLE_D90 VIPS_ANGLE_90
 #define VIPS_ANGLE_D180 VIPS_ANGLE_180
@@ -328,7 +325,7 @@ vips_watermark(VipsImage *in, VipsImage **out, WatermarkTextOptions *to, Waterma
 int
 vips_gaussblur_bridge(VipsImage *in, VipsImage **out, double sigma, double min_ampl) {
 #if (VIPS_MAJOR_VERSION == 7 && VIPS_MINOR_VERSION < 41)
-	return vips_gaussblur(in, out, ceil(sigma), NULL);
+	return vips_gaussblur(in, out, (int) sigma, NULL);
 #else
 	return vips_gaussblur(in, out, sigma, NULL, "min_ampl", min_ampl, NULL);
 #endif
