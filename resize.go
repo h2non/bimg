e6b50ded02b1a2be3014b20bdd62bacf03bd1d5b
Tomas Aparicio <tomas@aparicio.me>
2016-09-30 23:15:18 +0100

    refactor(resize): use not equal operator (again)

--- resize.go
+++ resize.go
@@ -242,7 +242,7 @@ func extractOrEmbedImage(image *C.VipsImage, o Options) (*C.VipsImage, error) {
 		left, top := (o.Width-inWidth)/2, (o.Height-inHeight)/2
 		image, err = vipsEmbed(image, left, top, o.Width, o.Height, o.Extend, o.Background)
 		break
-	case o.Top > 0 || o.Left > 0 || o.AreaWidth > 0 || o.AreaHeight > 0:
+	case o.Top != 0 || o.Left != 0 || o.AreaWidth != 0 || o.AreaHeight != 0:
 		if o.AreaWidth == 0 {
 			o.AreaHeight = o.Width
 		}
