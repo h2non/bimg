8f232ee92255b799200797dfb3cbea0a654209d1
Leo Adamek <leo.adamek@mrzen.com>
2017-03-14 14:22:42 +0000

    Fixed tests

--- image_test.go
+++ image_test.go
@@ -455,8 +455,8 @@ func TestFluentInterface(t *testing.T) {
 	Write("fixtures/test_image_fluent_out.png", image.Image())
 }
 
-func testImageAutoOrient(t *testing.T) {
-	image = initImage("exif_orientation_6.jpg")
+func TestImageAutoOrient(t *testing.T) {
+	image := initImage("exif_orientation_6.jpg")
 
 	_, err := image.AutoOrient()
 
@@ -464,7 +464,7 @@ func testImageAutoOrient(t *testing.T) {
 		t.Fatalf("Unable to auto-orient image: %#v", err)
 	}
 
-	Write("fixtures/exit_orientation_6_out.jpg", image.Image())
+	Write("fixtures/test_auto_orient_6.jpg", image.Image())
 }
 
 func initImage(file string) *Image {
