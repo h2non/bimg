cf75e90e480c3685e24d206028d68f548d829d68
Kishor Gandham <kishor@shopalyst.com>
2018-01-17 11:52:57 +0530

    Fix image crop during embed

--- resizer.go
+++ resizer.go
@@ -446,7 +446,11 @@ func imageCalculations(o *Options, inWidth, inHeight int) float64 {
 	switch {
 	// Fixed width and height
 	case o.Width > 0 && o.Height > 0:
-		factor = math.Min(xfactor, yfactor)
+		if o.Crop {
+			factor = math.Min(xfactor, yfactor)
+		} else {
+			factor = math.Max(xfactor, yfactor)
+		}
 	// Fixed width, auto height
 	case o.Width > 0:
 		if o.Crop {
