22d8a6f471c2adca16cb231c81857a90ca16b9ba
Adriano <adriano@caloiaro.com>
2018-05-19 12:30:06 -0400

    Add support for converting multi-page images
    - Initially support PDF, tiff, and gif (frames)

--- vips.go
+++ vips.go
@@ -76,6 +76,10 @@ type vipsWatermarkTextOptions struct {
 	Font *C.char
 }
 
+type vipsOptions struct {
+	PageNum C.int
+}
+
 func init() {
 	Initialize()
 }
@@ -293,6 +297,10 @@ func vipsWatermark(image *C.VipsImage, w Watermark) (*C.VipsImage, error) {
 }
 
 func vipsRead(buf []byte) (*C.VipsImage, ImageType, error) {
+	return vipsReadWithOptions(buf, Options{})
+}
+
+func vipsReadWithOptions(buf []byte, o Options) (*C.VipsImage, ImageType, error) {
 	var image *C.VipsImage
 	imageType := vipsImageType(buf)
 
@@ -303,7 +311,8 @@ func vipsRead(buf []byte) (*C.VipsImage, ImageType, error) {
 	length := C.size_t(len(buf))
 	imageBuf := unsafe.Pointer(&buf[0])
 
-	err := C.vips_init_image(imageBuf, length, C.int(imageType), &image)
+	opts := vipsOptions{PageNum: C.int(o.PageNum)}
+	err := C.vips_init_image(imageBuf, length, C.int(imageType), &image, (*C.Options)(unsafe.Pointer(&opts)))
 	if err != 0 {
 		return nil, UNKNOWN, catchVipsError()
 	}
