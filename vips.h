efd8ec7bd60e3184c7f6d4ffdbdecf729fe2219c
Andreas Schneider <andreas@staffbase.com>
2020-11-28 11:19:02 +0100

    🐛 Prevent freeing images too early when watermarking

--- vips.h
+++ vips.h
@@ -472,6 +472,7 @@ vips_watermark(VipsImage *in, VipsImage **out, WatermarkTextOptions *to, Waterma
 
 	VipsImage *base = vips_image_new();
 	VipsImage **t = (VipsImage **) vips_object_local_array(VIPS_OBJECT(base), 10);
+	g_object_ref(in);
 	t[0] = in;
 
 	// Make the mask.
@@ -562,6 +563,9 @@ int
 vips_watermark_image(VipsImage *in, VipsImage *sub, VipsImage **out, WatermarkImageOptions *o) {
 	VipsImage *base = vips_image_new();
 	VipsImage **t = (VipsImage **) vips_object_local_array(VIPS_OBJECT(base), 10);
+
+	g_object_ref(in);
+	g_object_ref(sub);
 
   // add in and sub for unreffing and later use
 	t[0] = in;
